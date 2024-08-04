88e9276eb1f098bfb064be2b6cef26ecf8c0ccbe
kingquincy <qwash2000@gmail.com>
2024-08-03 21:26:18 -0400

    Create script.js
    JavaScript taken from the index.html code and put into its own file

---
+++ script.js
@@ -0,0 +1,102 @@
+function bg1() {
+        document.body.style.backgroundImage = "url('foresthills.jpg')";
+        document.querySelector(".text1").style.opacity = 1;
+        document.querySelector(".artist1").style.opacity = 1;
+        // document.querySelector(".norole").play();
+        // document.querySelector(".norole").loop = true;
+
+    }
+
+    function bgleave1() {
+        document.querySelector(".text1").style.opacity = 0;
+        document.querySelector(".artist1").style.opacity = 0;
+        // document.querySelector(".norole").pause();
+    }
+
+    function bg2() {
+        document.body.style.backgroundImage = "url('coloringbook.jpg')";
+        document.querySelector(".text2").style.opacity = 1;
+        document.querySelector(".artist2").style.opacity = 1;
+    }
+
+    function bgleave2() {
+        document.querySelector(".text2").style.opacity = 0;
+        document.querySelector(".artist2").style.opacity = 0;
+    }
+
+    function bg3() {
+        document.body.style.backgroundImage = "url('darksky.jpg')";
+        document.querySelector(".text3").style.opacity = 1;
+        document.querySelector(".artist3").style.opacity = 1;
+    }
+
+    function bgleave3() {
+        document.querySelector(".text3").style.opacity = 0;
+        document.querySelector(".artist3").style.opacity = 0;
+    }
+
+    function bg4() {
+        document.body.style.backgroundImage = "url('graduation.jpg')";
+        document.querySelector(".text4").style.opacity = 1;
+        document.querySelector(".artist4").style.opacity = 1;
+    }
+
+    function bgleave4() {
+        document.querySelector(".text4").style.opacity = 0;
+        document.querySelector(".artist4").style.opacity = 0;
+    }
+
+    function bg5() {
+        document.body.style.backgroundImage = "url('coast.jpg')";
+        document.querySelector(".text5").style.opacity = 1;
+        document.querySelector(".artist5").style.opacity = 1;
+    }
+
+    function bgleave5() {
+        document.querySelector(".text5").style.opacity = 0;
+        document.querySelector(".artist5").style.opacity = 0;
+    }
+
+    function bg6() {
+        document.body.style.backgroundImage = "url('collegedropout.jpg')";
+        document.querySelector(".text6").style.opacity = 1;
+        document.querySelector(".artist6").style.opacity = 1;
+    }
+
+    function bgleave6() {
+        document.querySelector(".text6").style.opacity = 0;
+        document.querySelector(".artist6").style.opacity = 0;
+    }
+
+    function bg7() {
+        document.body.style.backgroundImage = "url('acidrap.jpg')";
+        document.querySelector(".text7").style.opacity = 1;
+        document.querySelector(".artist7").style.opacity = 1;
+    }
+
+    function bgleave7() {
+        document.querySelector(".text7").style.opacity = 0;
+        document.querySelector(".artist7").style.opacity = 0;
+    }
+
+    function bg8() {
+        document.body.style.backgroundImage = "url('bornsinner.jpg')";
+        document.querySelector(".text8").style.opacity = 1;
+        document.querySelector(".artist8").style.opacity = 1;
+    }
+
+    function bgleave8() {
+        document.querySelector(".text8").style.opacity = 0;
+        document.querySelector(".artist8").style.opacity = 0;
+    }
+
+    function bg9() {
+        document.body.style.backgroundImage = "url('section.jpg')";
+        document.querySelector(".text9").style.opacity = 1;
+        document.querySelector(".artist9").style.opacity = 1;
+    }
+
+    function bgleave9() {
+        document.querySelector(".text9").style.opacity = 0;
+        document.querySelector(".artist9").style.opacity = 0;
+    }
